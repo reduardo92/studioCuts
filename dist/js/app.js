7c754cfa2e091d6bfa7f3801761e5ed341eee213
eduardo rivas <r.eduardo92@gmail.com>
2019-08-26 17:44:04 -0500

    page done

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,17 +5,21 @@ const burger = document
   .querySelector('.burger-toggle')
   .addEventListener('click', e => {
     const body = document.body;
-    log(e.target);
     if (body.className === '') {
-      body.classList = '';
+      body.classList.remove('toggle');
       body.classList.add('toggle');
     } else {
-      body.classList = '';
+      body.classList.remove('toggle');
     }
   });
 
 // Get Nav links
-const navLinks = document.querySelectorAll('.nav--links__item');
+const nav = document.querySelector('.nav').addEventListener('click', e => {
+  const navLinks = document.querySelectorAll('.nav--links__item');
+  if (e.target.classList.contains('nav')) return;
+  navLinks.forEach(link => link.classList.remove('activeNav'));
+  e.target.classList.add('activeNav');
+});
 
 // Intersection Observer
 
@@ -24,8 +28,7 @@ const sections = document.querySelectorAll('.sections');
 
 ///// Sections Options
 const sectionsOptions = {
-  threshold: 0.25,
-  rootMargin: '-15%'
+  rootMargin: '-5%'
 };
 
 ///// Observer Function
@@ -34,8 +37,10 @@ const sectionsObserver = new IntersectionObserver(
     entries.forEach(entry => {
       if (!entry.isIntersecting) return;
 
+      if (window.screen.availWidth <= 768) return;
+
       // check if observer matches nav--link dataset
-      navLinks.forEach(link => {
+      document.querySelectorAll('.nav--links__item').forEach(link => {
         if (link.dataset.active === entry.target.id) {
           link.classList.remove('activeNav');
           link.classList.add('activeNav');
@@ -51,4 +56,21 @@ const sectionsObserver = new IntersectionObserver(
 // Observe section
 sections.forEach(sec => sectionsObserver.observe(sec));
 
-log(navLinks);
+// Modal Toggle
+const modalToggle = document.addEventListener('click', e => {
+  const body = document.body.classList;
+
+  if (e.target.classList.contains('qoute')) {
+    body.add('toogleModal');
+  } else if (
+    e.target.classList.contains('form--x') ||
+    e.target.classList.contains('form--modal')
+  ) {
+    body.remove('toogleModal');
+  }
+});
+
+// Form Button sumbmit
+const formBtn = document
+  .querySelector('.form--btn')
+  .addEventListener('click', e => e.preventDefault());
